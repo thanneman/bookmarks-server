9be1344ec91655a84c841289603de70767d5800f
thanneman <thanneman1@gmail.com>
2020-01-11 11:36:43 -0700

    added get and post for bookmark

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -5,3 +5,56 @@ const store = require('../store')
 
 const bookmarksRouter = express.Router()
 const bodyParser = express.json()
+
+bookmarksRouter
+    .route('/bookmarks')
+    .get((req, res) => {
+        res.json(store.bookmarks)
+    })
+    .post(bodyParser, (req, res) => {
+        const { title, url, description, rating } = req.body;
+
+        //check for title
+        if(!title) {
+            logger.error(`Title is required`);
+            return res
+                .status(400)
+                .send('Invalid data');
+        }
+
+        //check for url
+        if(!url) {
+            logger.error(`URL is required`);
+            return res
+                .status(400)
+                .send('Invalid data');
+        }
+
+        //check for description
+        if(!description) {
+            logger.error(`Description is required`);
+            return res
+                .status(400)
+                .send('Invalid data');
+        }
+
+        //check for rating
+        if(!rating) {
+            logger.error(`Rating is required`);
+            return res
+                .status(400)
+                .send('Invalid data');
+        }
+
+        //create an id
+        const id = uuid();
+        const bookmark = { id, title, url, description, rating }
+
+        store.bookmarks.push(bookmark)
+
+        logger.info(`Bookmark with id ${bookmark.id} created`)
+        res
+            .status(201)
+            .location(`http://localhost:8000/bookmarks/${bookmark.id}`)
+            .json(bookmark)
+    })
