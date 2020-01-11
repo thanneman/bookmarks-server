70023484f02ca42b89586371b2d6e729d376caf1
thanneman <thanneman1@gmail.com>
2020-01-11 11:13:10 -0700

    added logger and store

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const bookmarksRouter = require('./card/card-router')
 
 const app = express()
 
@@ -14,6 +15,19 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`);
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next()
+})
+
+app.use(bookmarksRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
@@ -27,7 +41,7 @@ app.use(function errorHandler(error, req, res, next) {
         console.log(error)
         response = { message: error.message, error }
     }
-    res.status(500).json(response)
-})
+    res.status(500).json(response);
+});
 
 module.exports = app
---
+++ src/bookmarks/bookmarks-router.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const uuid = require('uuid/v4')
+const logger = require('../logger')
+const store = require('../store')
+
+const bookmarksRouter = express.Router()
+const bodyParser = express.json()
