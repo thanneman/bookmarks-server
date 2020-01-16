ba65f598269cc1e3473aa4b2bccea043bf64ae47
thanneman <thanneman1@gmail.com>
2020-01-16 16:33:04 -0700

    refactored for database

--- migrations/001.undo.create_bookmarks.sql
+++ migrations/001.undo.create_bookmarks.sql
@@ -1,3 +1,1 @@
 DROP TABLE IF EXISTS bookmarks;
-
-DROP TABLE IF EXISTS bookmarks-test;
--- seeds/seed.bookmarks.sql
+++ seeds/seed.bookmarks.sql
@@ -2,4 +2,4 @@ INSERT INTO bookmarks (title, url, rating, description)
 VALUES
     ('Website #1', 'https://www.google.com', '5', 'Website #1 description'),
     ('Website #2', 'https://www.google.com', '1', 'Website #2 description'),
-    ('Website #3', 'https://www.google.com', '3', 'Website #3 description'),
+    ('Website #3', 'https://www.google.com', '3', 'Website #3 description');
