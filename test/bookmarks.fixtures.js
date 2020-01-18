77f85bb7136731d95ceaf654097902f8808bb2e7
thanneman <thanneman1@gmail.com>
2020-01-18 09:33:15 -0700

    All tests passing

---
+++ test/bookmarks.fixtures.js
@@ -0,0 +1,29 @@
+function makeBookmarksArray() {
+    return [
+      {
+        id: 1,
+        title: 'Website #1',
+        url: 'https://www.google.com',
+        rating: 5,
+        description: 'Website #1 description',
+      },
+      {
+        id: 2,
+        title: 'Website #2',
+        url: 'https://www.google.com',
+        rating: 5,
+        description: 'Website #2 description',
+      },
+      {
+        id: 3,
+        title: 'Website #3',
+        url: 'https://www.google.com',
+        rating: 5,
+        description: 'Website #3 description',
+      },
+    ];
+  }
+  
+  module.exports = {
+    makeBookmarksArray,
+  }
